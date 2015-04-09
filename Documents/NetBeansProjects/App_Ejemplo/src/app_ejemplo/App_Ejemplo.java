7d83b2bb697be3c9e72e37655f1a400d9cbbcf51
dannyel-34 <dcalle338@gmail.com>
2015-04-08 20:55:48 -0500

    Archivos modificados

---
+++ Documents/NetBeansProjects/App_Ejemplo/src/app_ejemplo/App_Ejemplo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app_ejemplo;
+
+/**
+ *
+ * @author DANIEL
+ */
+public class App_Ejemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hola, estoy creando un repositorio de git en githup");
+    }
+    
+}
