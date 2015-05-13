abd5ae7ca71df916bfe338da6584a36a27ead2e3
dannyel-34 <dcalle338@gmail.com>
2015-05-12 21:47:57 -0500

    Agrege las lineas en la clase DBConexion

---
+++ Documents/NetBeansProjects/Mascotas/src/java/PaqueteConexion/DBConexion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PaqueteConexion;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.exceptions.MySQLDataException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author DANIEL
+ */
+
+public class DBConexion {
+   
+    Connection link = null;
+    public String db = "dbMascotas";
+    public String host = "jdbc:mysql://localhost:3306/";
+    public String user = "root";
+    public String pass = "";
+
+    String url = host + db + "?user=" + user + "&password="+pass;
+ 
+    public Connection Conectar(){
+
+       
+       try{
+           Class.forName("com.mysql.jdbc.Driver");
+           Connection conn = (Connection) DriverManager.getConnection(url);
+           conn.close();
+       }catch(ClassNotFoundException e){
+           System.out.println("De de conexion");
+       }catch(SQLException e){
+           System.out.println("Error SQL");
+       }          
+       return link;
+   }     
+}
